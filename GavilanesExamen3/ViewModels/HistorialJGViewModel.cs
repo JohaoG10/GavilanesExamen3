23b58a25b1595541a9f4f521386486221240fcca
JohaoG10 <johao.gavilanes@udla.edu.ec>
2025-01-20 14:16:43 -0500

    Data Historial

---
+++ GavilanesExamen3/ViewModels/HistorialJGViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.ObjectModel;
+using System.Threading.Tasks;
+
+public class HistorialJGViewModel : BindableObject
+{
+    public ObservableCollection<PaisJG> Paises { get; set; }
+
+    public HistorialJGViewModel()
+    {
+        LoadData();
+    }
+
+    private async Task LoadData()
+    {
+        Paises = new ObservableCollection<PaisJG>(await App.Database.GetItemsAsync());
+    }
+}
