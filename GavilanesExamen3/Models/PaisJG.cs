a100d17040a03629c220d0c4099f7ce8e166fdd7
JohaoG10 <johao.gavilanes@udla.edu.ec>
2025-01-20 14:01:05 -0500

    Agregar archivos de proyecto.

---
+++ GavilanesExamen3/Models/PaisJG.cs
@@ -0,0 +1,11 @@
+﻿using SQLite;
+
+public class PaisJG
+{
+    [PrimaryKey, AutoIncrement]
+    public int Id { get; set; }
+    public string NombreOficial { get; set; }
+    public string Region { get; set; }
+    public string GoogleMapsLink { get; set; }
+    public string NombreUsuario { get; set; } 
+}
